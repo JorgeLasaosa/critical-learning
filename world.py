5a620868f41d798ae1b20b23642d345a93c91327
JorgeLasaosa <georgelasaosa@gmail.com>
2016-11-21 01:56:09 +0100

    Added non-periodic world

--- world.py
+++ world.py
@@ -78,9 +78,10 @@ class robot(object):
 											
 # Define a 2D world nRows x nCols
 class world:
-	def __init__(self, nRows, nCols):	
+	def __init__(self, nRows, nCols, periodic=True):	
 		self.nRows = nRows
 		self.nCols = nCols
+		self.periodic = periodic
 		self.M = np.empty((nRows, nCols), dtype=object)
 	
 	# Place an object in the world
@@ -91,7 +92,17 @@ class world:
 	
 	# Update the position of an object int he world
 	def updateObjectPosition(self, obj, newCoords):
-		newCoords = tuple(np.array(newCoords) % np.array((self.nRows,self.nCols)))
+		if self.periodic :
+			newCoords = tuple(np.array(newCoords) % np.array((self.nRows,self.nCols)))
+		else:
+			if newCoords[0] >= self.nRows:
+				newCoords = (self.nRows-1, newCoords[1])
+			elif newCoords[0] < 0:
+				newCoords = (0, newCoords[1])
+			if newCoords[1] >= self.nCols:
+				newCoords = (newCoords[0], self.nCols-1)
+			elif newCoords[1] < 0:
+				newCoords = (newCoords[0], 0)
 		newSquare = self.M[newCoords]		# Object placed at new square
 		if newSquare is None:
 			self.M[obj.coords] = None
@@ -100,10 +111,28 @@ class world:
 		elif newSquare.pushable:
 		# Push the object and take the square	
 			push = tuple(np.subtract(newSquare.coords, obj.coords))
-			newSquare.coords = tuple(np.array(np.add(newSquare.coords,push)) % np.array((self.nRows,self.nCols)))
-			self.M[newSquare.coords] = newSquare
-			self.M[newCoords] = obj
-			self.M[obj.coords] = None
-			obj.coords = newCoords
+			pushedPosition = np.array(np.add(newSquare.coords,push))
+			if self.periodic:
+				newSquare.coords = tuple(pushedPosition % np.array((self.nRows,self.nCols)))
+			else:
+				objectWillBePushed = True
+				if pushedPosition[0] >= self.nRows:
+					pushedPosition[0] = self.nRows-1
+					objectWillBePushed = False
+				elif pushedPosition[0] < 0:
+					pushedPosition[0] = 0
+					objectWillBePushed = False
+				if pushedPosition[1] >= self.nCols:
+					pushedPosition[1] = self.nCols-1
+					objectWillBePushed = False
+				elif pushedPosition[1] < 0:
+					pushedPosition[1] = 0
+					objectWillBePushed = False
+			
+			if objectWillBePushed:
+				self.M[newSquare.coords] = newSquare
+				self.M[newCoords] = obj
+				self.M[obj.coords] = None
+				obj.coords = newCoords
 			
 		
